73c4501e38479529ea671165f1e3add0e66aef52
An3s079 <68040208+An3s079@users.noreply.github.com>
2021-06-18 22:19:13 -1000

    Add files via upload

---
+++ DPSTeller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DPSMod
+{
+	class DPSTeller : MonoBehaviour
+	{
+		void Update()
+		{
+			
+			DPSmod.DPsText.text = "DPS: " + DPSmod.getDPS();
+		}
+	}
+}
